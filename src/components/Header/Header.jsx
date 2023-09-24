63d34883be425ad33ceb304f9a7f20ab554e12ea
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-09-24 11:52:53 +0600

    Active NavLink style set by dynamic class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
                 <div className="flex justify-between items-center">
                     <h2 className="text-3xl font-extrabold">CareerHub</h2>
                     <div className="flex flex-row gap-10 ">
-                        <NavLink to="/">Statistics</NavLink>
-                        <NavLink to="/appliedjobs">Applied Jobs</NavLink>
-                        <NavLink to='/blog'>Blog</NavLink>
+                        <NavLink to="/" className={({isActive, isPending})=>isPending?'pending':isActive?'text-green-500 underline':''}>Statistics</NavLink>
+                        <NavLink to="/appliedjobs" className={({isActive, isPending})=>isPending?'pending':isActive?'text-green-500 underline':''}>Applied Jobs</NavLink>
+                        <NavLink to='/blog' className={({isActive, isPending})=>isPending?'pending':isActive?'text-green-500 underline':''}>Blog</NavLink>
                     </div>
                     <button className="btn btn-primary">Star Applying</button>
                 </div>
